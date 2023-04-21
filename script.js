d2cc120b8ca7185b49bf9a8979d3ff62508bc705
Zeta Code <129629733+Zetacodeid@users.noreply.github.com>
2023-04-21 18:20:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function wa() {
+    window.location.href = "https://wa.me/message/BZP6TBJ6VVQYP1";
+}
+
+window.onload = function () {
+    setTimeout(function () {
+        document.querySelector(".loading").style.display = "none";
+    }, 5000);
+};
